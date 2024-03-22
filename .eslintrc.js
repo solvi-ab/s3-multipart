e3bacb250f826fd22019a1af5766794454637f24
Per Liedman <per@liedman.net>
2024-03-22 09:59:48 +0100

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ["standard", "prettier"],
+  globals: {},
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: [],
+  settings: {},
+  overrides: [],
+};
